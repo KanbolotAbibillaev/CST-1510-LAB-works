97b153101114bf5291e0161ff915e67ca1d06d5c
ThirdrReichSS <kanbolot.004@gmail.com>
2025-11-28 11:08:15 +0400

    LAB_1.py

---
+++ LAB_1.py
@@ -0,0 +1,44 @@
+"""Threshold 1 Questionnaire"""
+# name = input("Enter your name: ")
+# interest = input("Enter your interest: ")
+# nation = input("Enter your nation: ")
+# course = input("Enter your course: ")
+# exp = input("Enter your experience: ")
+# print(f'Name: {name}')
+# print(f'Interest: {interest}')
+# print(f'Nation: {nation}')
+# print(f'Course: {course}')
+# print(f'Experience: {exp}')
+"""OUTPUT"""
+# Name: Kanbolot
+# Interest: Coding
+# Nation: Kyrgyz
+# Course: Cybersecurity
+# Experience: 1 year
+
+"""Threshold 2 Rectangle Area & Perimeter"""
+# width = 4.5
+# height = 7.9
+# area = round(width * height,2)
+# perimeter = round((2 * width + height), 2)
+# print('area: ', area)
+# print('perimeter', perimeter)
+"""OUTPUT"""
+# area:  35.55
+# perimeter 16.9
+
+"""Threshold 3 Convert Pounds to Kilograms"""
+# lb = float(input("Enter lb: "))
+# kg = round(lb * 0.454, 1)
+# print('KG: ', kg)
+"""OUTPUT"""
+# Enter lb: 228
+# KG:  103.5
+"""Threshold 4 Convert Feet to Meters"""
+feet = float(input('Enter a feet: '))
+meters = feet * 0.305
+print('Meters: ', meters)
+"""OUTPUT"""
+# Enter a feet: 34
+# Meters:  10.37
+
